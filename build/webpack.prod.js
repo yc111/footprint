dffbb767a92a035112b1855a8808435cf1e9b731
yc111 <yinchuan_111@163.com>
2019-09-26 18:21:05 +0800

    add Travis CI config

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,11 +1,18 @@
+const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const TersorWebpackPlugin = require('terser-webpack-plugin');
 
+console.log(path.join(process.cwd()));
 module.exports = {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin({}),
+        function () {
+            this.plugin('done', function (stats) {
+                console.log('stats');
+            });
+        }
     ],
     optimization: {
         minimizer: [
